e2fdcb48192da4d892a7ab4875f57945f3b112e3
Bhavin Patil <59190105+bhavinpatil@users.noreply.github.com>
2023-03-09 23:51:24 +0530

    Added front end and integrated model with the web

---
+++ app.py
@@ -0,0 +1,53 @@
+from flask import Flask, render_template, request
+from webScraping import WebScraping
+from mlmodel import Ratings
+import pickle
+
+app = Flask(__name__)
+
+CourseInfo = {
+    'totalCourses': 0,
+    'courses': []
+}
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/analysis',methods=['GET','POST'])
+def analysis():
+    if request.method == 'GET':
+        return render_template('analysisForm.html')
+    
+    if request.method == 'POST':
+        link = request.form.get('link')
+        site = request.form.get('platform')
+        CourseInfo['totalCourses'] = 1
+        CourseInfo['courses'].clear() 
+        CourseInfo['courses'].append(WebScraping(link,site))
+        CourseInfo['courses'][0]['rating'] = Ratings(CourseInfo['courses'][0]['comments'])
+        CourseInfo['courses'][0]['platform'] = site
+        CourseInfo['courses'][0]['link'] = link
+        return render_template('analysisReport.html',CourseInfo=CourseInfo)
+
+@app.route('/multipleAnalysis',methods=['GET','POST'])
+def multipleAnalysis():
+    if request.method == 'POST':
+        if request.form.get('platform'):
+            link = request.form.get('link')
+            site = request.form.get('platform')
+            CourseInfo['totalCourses'] += 1
+            CourseInfo['courses'].append(WebScraping(link,site))
+
+            # We will load the model here and after calling it with list of comments as parameter it should return rating
+            # CourseInfo['courses'][-1]['rating'] = model(CourseInfo.comments)
+
+            CourseInfo['courses'][-1]['rating'] = Ratings(CourseInfo['courses'][-1]['comments'])
+            CourseInfo['courses'][-1]['platform'] = site
+            CourseInfo['courses'][-1]['link'] = link
+        return render_template('multipleAnalysisReport.html',CourseInfo=CourseInfo)
+    
+
+@app.route('/aboutus')
+def aboutus():
+    return render_template('aboutUs.html')
---
+++ webScraping.py
@@ -0,0 +1,55 @@
+from bs4 import BeautifulSoup
+import urllib.request, json
+import requests
+
+
+def WebScraping(link, platform):
+    site = platform
+    c_url = link
+    comments = []
+    c = []
+    result = {}
+    if site =="Youtube":
+        result['v'] = link[link.index('=')+1:]
+        videoId = c_url[(c_url.index("=") + 1):]
+        url = 'https://youtube.googleapis.com/youtube/v3/videos?part=contentDetails&id={}&key={}'.format(
+            videoId, 'AIzaSyAId96TrNTCJd3NJRUrqun-SbiS3eWQ8rM')
+        response = urllib.request.urlopen(url)
+        data = response.read()
+        data = json.loads(data)
+        duration = data["items"][0]["contentDetails"]["duration"]
+        result["duration"] = ''
+        if duration.find('H')!=-1:
+            result["duration"] = result["duration"] + (duration[duration.find('T') + 1:duration.find('H')] + ' Hr ')
+        if duration.find('M')!=-1 and duration.find('H')!=-1:
+            result["duration"] = result["duration"] + ' ' + (duration[duration.find('H') + 1:duration.find('M')] + ' Mins')
+        elif duration.find('M')!=-1:
+            result["duration"] = result["duration"] + ' ' +(duration[duration.find('T') + 1:duration.find('M')] + ' Mins')
+
+        url = 'https://youtube.googleapis.com/youtube/v3/videos?part=statistics&id={}&key={}'.format(
+            videoId, 'AIzaSyAId96TrNTCJd3NJRUrqun-SbiS3eWQ8rM')
+        response = urllib.request.urlopen(url)
+        data = response.read()
+        data = json.loads(data)
+        result["learner_count"] = data["items"][0]["statistics"]["viewCount"] + ' views'
+
+        url = 'https://youtube.googleapis.com/youtube/v3/videos?part=snippet&id={}&key={}'.format(
+            videoId, 'AIzaSyAId96TrNTCJd3NJRUrqun-SbiS3eWQ8rM')
+        response = urllib.request.urlopen(url)
+        data = response.read()
+        data = json.loads(data)
+        result["platform"] = "YouTube"
+        result["title"] = data["items"][0]["snippet"]["title"]
+        result["instructor"] = data["items"][0]["snippet"]["channelTitle"]
+        result["description"] = data["items"][0]["snippet"]["description"]
+
+        url = 'https://youtube.googleapis.com/youtube/v3/commentThreads?part=snippet&maxResults=100&videoId={}&key={}'.format(
+            videoId, 'AIzaSyAId96TrNTCJd3NJRUrqun-SbiS3eWQ8rM')
+        response = urllib.request.urlopen(url)
+        data = response.read()
+        data = json.loads(data)
+        for i in range(len(data["items"])):
+            if data['items'][i]['snippet']['topLevelComment']['snippet']['textDisplay'] is not None:
+                comments.append(data['items'][i]['snippet']['topLevelComment']['snippet']['textDisplay'])
+        result["comments"] = comments
+        return result
